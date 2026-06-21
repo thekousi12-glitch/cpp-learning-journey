ac7e6a5da3b7c1140f658e92899f70e3228a0298
thekousi12-glitch <thekousi12@gmail.com>
2026-06-21 16:11:21 +0530

    added files
    applying what I've learned so far

---
+++ HelloWorld.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()       // int defines the function type i.e. "integer", main is the function.
+{
+	std::cout << "Leena Banda"; //std::cout lives in iostream
+	return 0; //programme returns the value 0 to the OS, which means the programme has run successfully
+
+}
---
+++ iostream.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+
+	int x{ 4 };
+
+	std::cout << x  <<"resilience"  <<3 ;
+
+	return 0;
+
+
+}
